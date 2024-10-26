62f7795804f49d23bf7cea75d58ae72204297eec
Matt Kibbler <mattkibbler@gmail.com>
2024-10-26 17:14:34 +0100

    Serialize product cache into JSON file and unserialize on server start

--- cmd/main.go
+++ cmd/main.go
@@ -13,6 +13,7 @@ var prodCache sync.Map
 
 func main() {
 	listenAddr := ":8080"
+	productCachePath := "product_cache.json"
 
 	mux := http.NewServeMux()
 
@@ -20,12 +21,28 @@ func main() {
 	mux.HandleFunc("GET /product/{id}", shop.HandleGetProduct)
 
 	go func() {
+		log.Println("Attempting to unserialize product cache...")
+		pCount, err := shop.UnserializeProductCache(productCachePath, &prodCache)
+		log.Printf("%d products unserialized\n", pCount)
+		if err != nil {
+			log.Printf("error unserializing product cache: %v\n", err)
+		}
+		// If we unserialized some products, we don't need to fetch them from the API straight away
+		// This is mainly so we're not hitting the API every time we stop/start the server during development
+		if pCount > 0 {
+			time.Sleep(10 * time.Minute)
+		}
 		for {
 			log.Println("Fetching products...")
 			err := shop.FetchAndCacheProducts(&prodCache)
 			if err != nil {
 				log.Printf("error fetching products: %v\n", err)
 			}
+			err = shop.SerializeProductCache(productCachePath, &prodCache)
+			if err != nil {
+				log.Printf("error serializing product cache: %v\n", err)
+			}
+
 			// Schedule product refresh after a delay
 			time.Sleep(10 * time.Minute)
 		}
--- internal/shop/tasks.go
+++ internal/shop/tasks.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 )
@@ -40,4 +41,60 @@ func FetchAndCacheProducts(productCache *sync.Map) error {
 	}
 
 	return nil
+}
+
+func SerializeProductCache(filePath string, productCache *sync.Map) error {
+	tempMap := make(map[string]Product)
+
+	productCache.Range(func(key, value interface{}) bool {
+		k, ok := key.(string)
+		if !ok {
+			return false
+		}
+		v, ok := value.(Product)
+		if !ok {
+			return false
+		}
+		tempMap[k] = v
+		return true
+	})
+
+	data, err := json.Marshal(tempMap)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filePath, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UnserializeProductCache(productCachePath string, productCache *sync.Map) (int, error) {
+	// check file exists, if not then this is probably the first time the server has ran
+	if _, err := os.Stat(productCachePath); os.IsNotExist(err) {
+		return 0, nil
+	}
+
+	// read file data
+	data, err := os.ReadFile(productCachePath)
+	if err != nil {
+		return 0, fmt.Errorf("failed to read product cache file: %v", err)
+	}
+
+	// deserialize JSON into an ordinary map
+	tempMap := make(map[string]Product)
+	if err := json.Unmarshal(data, &tempMap); err != nil {
+		return 0, fmt.Errorf("failed to deserialize product cache: %v", err)
+	}
+
+	// restore data into the original sync.map
+	pCount := 0
+	for key, value := range tempMap {
+		productCache.Store(key, value)
+		pCount++
+	}
+	return pCount, nil
 }
